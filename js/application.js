7c0c02701ba3b0eac6627ef44305f6fe82061bf0
mosk <miiixsc@gmail.com>
2017-02-13 13:57:35 +0300

    burger

--- js/application.js
+++ js/application.js
@@ -1,8 +1,15 @@
 $(document).ready(function() {
-  var menu = $('<ul class="menu-burger"><li class="menu-burger__item"><a class="menu-burger__link" href="#about">О нас</a></li><li class="menu-burger__item"><a class="menu-burger__link menu-burger__link--active" href="#projects">Проекты</a></li><li class="menu-burger__item"><a class="menu-burger__link" href="#about">Услуги</a></li><li class="menu-burger__item"><a class="menu-burger__link" href="#contacts">Контакты</a></li></ul>');
-  menu.prependTo($('.header'));
   $('.menu').remove();
-  $('.menu-burger').css({'top': '0', 'margin-top': '0'});
-  var menu__button = $('<button class="button-close">Закрыть меню</button>');
-  menu__button.prependTo($('.menu-burger'));
+  $('.burger').on('click', function(){
+    var menu = $('<ul class="menu-burger"><li class="menu-burger__item"><a class="menu-burger__link" href="#about">О нас</a></li><li class="menu-burger__item"><a class="menu-burger__link" href="#projects">Проекты</a></li><li class="menu-burger__item"><a class="menu-burger__link" href="#about">Услуги</a></li><li class="menu-burger__item"><a class="menu-burger__link" href="#contacts">Контакты</a></li></ul>');
+    menu.prependTo($('.header'));
+    var menu__button = $('<button class="button-close">Закрыть меню</button>');
+    menu__button.prependTo($('.menu-burger'));
+    $('.button-close').on('click', function() {
+      menu.remove();
+    });
+    $('.menu-burger__link').on('click', function() {
+      menu.remove();
+    });
+  });
 });
