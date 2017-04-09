78d434f2094833ebde96ae87a0847e4fa9dcecee
mosk <miiixsc@gmail.com>
2017-04-09 20:19:34 +0300

    menu--burger

--- js/application.js
+++ js/application.js
@@ -1,15 +1,31 @@
-$(document).ready(function() {
-  $('.menu').remove();
-  $('.burger').on('click', function(){
-    var menu = $('<ul class="menu-burger"><li class="menu-burger__item"><a class="menu-burger__link" href="#about">О нас</a></li><li class="menu-burger__item"><a class="menu-burger__link" href="#projects">Проекты</a></li><li class="menu-burger__item"><a class="menu-burger__link" href="#about">Услуги</a></li><li class="menu-burger__item"><a class="menu-burger__link" href="#contacts">Контакты</a></li></ul>');
-    menu.prependTo($('.header'));
-    var menu__button = $('<button class="button-close">Закрыть меню</button>');
-    menu__button.prependTo($('.menu-burger'));
-    $('.button-close').on('click', function() {
-      menu.remove();
-    });
-    $('.menu-burger__link').on('click', function() {
-      menu.remove();
-    });
+var buttonOpen = $('<button class="hamburger hamburger--collapse burger" type="button"><span class="hamburger-box"><span class="hamburger-inner"></span></span></button>');
+var buttonClose = $('<button class="burger button--close">Меню</button>');
+
+function showBurger() {
+  $('.menu').removeClass('menu').addClass('menu-burger');
+  $('.menu-burger').prependTo($('.header'));
+  buttonOpen.prependTo($('.lang'));
+  buttonClose.prependTo($('.menu-burger'));
+  $('.burger').on('click', function() {
+    $('.menu-burger').slideToggle();
   });
+}
+
+function showNormalMenu() {
+  buttonOpen.remove();
+  buttonClose.remove();
+  $('.menu-burger').removeClass('menu-burger').addClass('menu');
+  $('.menu').prependTo($('.nav'));
+}
+
+function windowSize() {
+  if ($(window).width() <= '724') {
+    showBurger();
+  } else {
+    showNormalMenu();
+  }
+}
+
+$(document).ready(function() {
+  $(window).on('load resize', windowSize);
 });
